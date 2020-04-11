df7f87abd6f13b8a2fd93d85e072af59c6d17db3
Mishan Madhupa <mishancool2016@gmail.com>
2020-04-11 23:57:56 +0530

    Start front end

---
+++ srilanka_trip_client/srilankaTrip/src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+    _id?: string;
+    username?: string;
+    email: string;
+    // firstName: string;
+    // lastName: string;
+    password?: string;
+    // avatar?: string;
+    // address: string;
+    // city: string;
+    // role?: string;
+    // phone?: number;
+    // active?: Boolean;
+}
