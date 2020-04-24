bc34a700de25b55fdbba7831885c3dd6a003474f
Mishan Madhupa <mishancool2016@gmail.com>
2020-04-24 13:35:34 +0530

    Conect with mongodb database

--- srilanka_trip_server/app.js
+++ srilanka_trip_server/app.js
@@ -5,16 +5,46 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+const config = require('./config/database');
+
+// establish database connection
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: ' + config.database);
+});
+//on error
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
+
+// custom routes  **********
+const users = require('./routes/users');
+
 const app = express();
 
-// routes
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// serve resources from public folder
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+// routes  **********
 app.get('/', (req, res) => {
     res.send('Sri Lanka Trip API');
 });
+app.use('/users', users);
+
 
 
 const port = 3002;
 
+// Start server
 app.listen(port, () => {
     console.log('Server started on port: ' + port);
 });
